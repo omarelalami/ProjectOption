2f4fa8712fa7878437d5b467206c8096d80f33cc
omarelalami <83217646+omarelalami@users.noreply.github.com>
2021-12-06 11:48:20 +0100

    Update Program.cs

--- ProjectOption/Program.cs
+++ ProjectOption/Program.cs
@@ -19,42 +19,47 @@ namespace ProjectOption
             op.range();
             et.setPetud("","","",1);
             et.setPetud("","","",2);
-            et.setPetud("","","",3);
-            List<string> libre = new List<string>(); //Liste des etudiante qui ont pas encore obtenir une place
-
-            for (int i = 0; i < et.getnbE(); i++)
-            {
-
-
-                libre.Add(et.getnom(i));
-
-            }
-           
-            string[,] res = new string[et.getnbE(), 2];
+            et.setPetud("","","",3);       
+            
             op.setnbA(20);
             int nbA = op.getnbA();
             op.setnbG(20);
             int nbG = op.getnbG();
             op.setnbR(20);
             int nbR = op.getnbR();
-
-           int ir = 0;
-            int jr;
-            int je = 0;
-            int r = 0;
-            int n = 0;
-            while (libre.Any())
+          ////********En haut juste un implémentetion de classe*********\\\\ 
+          
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          
+          
+          /////////////////////////////////********En bas  le début de l'algorithme de gale sheaply pour Sélectioner l'étudiante et leur choi *********\\\\
+         
+          List<string> libre = new List<string>(); //Liste des étudiante qui ont pas encore obtenir une place  
+          for (int i = 0; i < et.getnbE(); i++)  /////En affecter le nom toute des étudiante de la classe
             {
-                for (int i = 1; i < 4; i++)
+
+
+                libre.Add(et.getnom(i));
+
+            }
+          string[,] res = new string[et.getnbE(), 2]; //le Tableau res de deux dimension retourne  un paire de nom et de option qui mérite l'tudiante
+           int ir = 0;  //une indice de la table res
+            int jr; //une indice de la table res
+            int je = 0;
+            int r = 0; //une indice du suppression sur la liste
+            int n = 0; //une indice de la table Petud qui stcoker le nom de l'étudiante et leurs trois choix
+            while (libre.Any())   ////La boucle While arreter si la liste des étudiante est vide
+            {
+                for (int i = 1; i < 4; i++)   
 
 
                 {
                     jr = 0;
-                    if ((et.Petud[je, i] == "abd") && nbA != 0)
-                    {
-                        res[ir, jr] = et.Petud[n, 0];
-                        jr++;
-                        res[ir, jr] = "abd";
+                    if ((et.Petud[je, i] == "abd") && nbA != 0)  //si l'étudiante choisir l'option abd et il ya des place sur cette option alors affcter a la table rses le choi et le nom
+                    {                                            //et la boucle va arreter et le nombre des palces décrementer par 1
+                        res[ir, jr] = et.Petud[n, 0]; //affectation  de nom
+                        jr++; //la varible est incremente pour affecter la paire /l'option)
+                        res[ir, jr] = "abd";   //affectation de l'option
                         nbA--;
                         i = 4;
 
@@ -63,7 +68,7 @@ namespace ProjectOption
 
 
                     }
-                    else if (et.Petud[je, i] == "gl" && nbG != 0)
+                    else if (et.Petud[je, i] == "gl" && nbG != 0) sinon GL la meme procedure pour la condtion if
                     {
                         res[ir, jr] = et.Petud[n, 0];
                         jr++;
@@ -72,7 +77,7 @@ namespace ProjectOption
                         i = 4;
 
                     }
-                    else if (et.Petud[je, i] == "asr" && nbR != 0)
+                    else if (et.Petud[je, i] == "asr" && nbR != 0)  sinon GL la meme procedure pour la condtion else if
                     {
                         res[ir, jr] = et.Petud[n, 0];
                         jr++;
@@ -95,7 +100,7 @@ namespace ProjectOption
                 n++;
 
             }
-            for (int i = 0; i < et.getnbE(); i++)
+            for (int i = 0; i < et.getnbE(); i++)  ///Affichge de résultat
 
             {
                 Console.Write(res[i, 0] + "\t");
